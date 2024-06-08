bd11612635ea283088103490a4d37bb4e8aae789
shahadat <nayemshahadat581@gmail.com>
2024-06-08 16:06:58 +0600

    make user admin api done

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    
+
     const userCollection = client.db('catro').collection('users')
     const patCollection = client.db('catro').collection('pat')
     const adoptCollection = client.db('catro').collection('adopted')
@@ -39,27 +39,27 @@ async function run() {
 
       jwt.verify(token, process.env.DB_TOKEN, (err, decoded) => {
         if (err) {
-          return res.status(401).send({ message: "Unauthorize access"})
+          return res.status(401).send({ message: "Unauthorize access" })
         }
         req.decoded = decoded;
         next()
       })
     }
 
-    app.post('/donators',async (req,res)=>{
-         const info=req.body;
-         const result=await donatorstCollection.insertOne(info)
-         res.send(result)
+    app.post('/donators', async (req, res) => {
+      const info = req.body;
+      const result = await donatorstCollection.insertOne(info)
+      res.send(result)
     })
-    app.get('/donators',verifyToken,async (req,res)=>{
-       const result =await donatorstCollection.find().toArray()
-       res.send(result)
+    app.get('/donators', verifyToken, async (req, res) => {
+      const result = await donatorstCollection.find().toArray()
+      res.send(result)
     })
-    app.delete('/donators/:id', async (req,res)=>{
-       const id=req.params.id;
-       const query = { _id: new ObjectId(id) }
-       const result =await donatorstCollection.deleteOne(query)
-       res.send(result)
+    app.delete('/donators/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await donatorstCollection.deleteOne(query)
+      res.send(result)
     })
     app.get('/pat', async (req, res) => {
       const mySort = { date: -1 }
@@ -67,14 +67,14 @@ async function run() {
       res.send(result)
     })
 
-    app.delete('/petdelete/:id',async (req,res)=>{
-          const id=req.params.id;
-          const query = { _id: new ObjectId(id) }
-          const result=await patCollection.deleteOne(query)
-          res.send(result)
+    app.delete('/petdelete/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await patCollection.deleteOne(query)
+      res.send(result)
     })
     app.get('/pet', verifyToken, async (req, res) => {
-      const mySort = {date: -1}
+      const mySort = { date: -1 }
       const result = await patCollection.find().sort(mySort).toArray()
       res.send(result)
     })
@@ -84,90 +84,90 @@ async function run() {
       const result = await patCollection.findOne(query)
       res.send(result)
     })
-    app.put('/updatepet/:id',verifyToken,async (req,res)=>{
-         const id=req.params.id;
-         const update=req.body;
-         
-         const query={_id:new ObjectId(id)}
-         const options={upsert:true}
-         const updateUser={
-          $set:{
-                name:update.petName,
-                category:update.petCategory,
-                age:update.petAge,
-                date:update.date,
-                location:update.location,
-                image:update.image,
-                email:update.email,
-                title:update.shortDes,
-                description:update.description,
-          }
-       }
-       const result=await patCollection.updateOne(query,updateUser,options)
-       res.send(result)
+    app.put('/updatepet/:id', verifyToken, async (req, res) => {
+      const id = req.params.id;
+      const update = req.body;
+
+      const query = { _id: new ObjectId(id) }
+      const options = { upsert: true }
+      const updateUser = {
+        $set: {
+          name: update.petName,
+          category: update.petCategory,
+          age: update.petAge,
+          date: update.date,
+          location: update.location,
+          image: update.image,
+          email: update.email,
+          title: update.shortDes,
+          description: update.description,
+        }
+      }
+      const result = await patCollection.updateOne(query, updateUser, options)
+      res.send(result)
     })
 
-   app.post('/createcamp',async (req,res)=>{
-       const info=req.body;
-       const result=await campaigntCollection.insertOne(info)
-       res.send(result)
-   })
-   app.put('/updatecampaign/:id',verifyToken,async (req,res)=>{
-    const id=req.params.id;
-    const update=req.body;
-    
-    const query={_id:new ObjectId(id)}
-    const options={upsert:true}
-    const updateUser={
-     $set:{
-         category:update.category,
-         email:update.email,
-         pet_image:update.image,
-         maximum_donation:update.max_donation,
-         last_date_of_donation:update.last_date,
-         short_description:update.shorts_des,
-     }
-  }
-  const result=await campaigntCollection.updateOne(query,updateUser,options)
-  res.send(result)
-})
-    app.patch('/update/status/:id',async (req,res)=>{
-        const id=req.params.id;
-        const query={_id:new ObjectId(id)}
-        const options={upsert:true}
-        const updateStatus={
-          $set:{
-             status:'Adopted',
-          }
-        }
-        const result =await patCollection.updateOne(query,updateStatus,options)
-        res.send(result)
-    })
-    app.put('/update/campaign/status/:id',verifyToken,async (req,res)=>{
-      const id=req.params.id;
-      const query={_id:new ObjectId(id)}
-      const options={upsert:true}
-      const info=await campaigntCollection.findOne(query)
-      let stat='';
-      if(info.status==='unpause'){
-        stat='pause'
-      }
-      else{
-        stat='unpause'
-      }
-      const updateStatus={
-        $set:{
-           status:stat,
-        }
-      }
-      const result =await campaigntCollection.updateOne(query,updateStatus,options)
+    app.post('/createcamp', async (req, res) => {
+      const info = req.body;
+      const result = await campaigntCollection.insertOne(info)
       res.send(result)
-  })
-    app.post('/addpet', async (req,res)=>{
-         const info=req.body;
-         console.log(info)
-         const result=await patCollection.insertOne(info)
-         res.send(result)
+    })
+    app.put('/updatecampaign/:id', verifyToken, async (req, res) => {
+      const id = req.params.id;
+      const update = req.body;
+
+      const query = { _id: new ObjectId(id) }
+      const options = { upsert: true }
+      const updateUser = {
+        $set: {
+          category: update.category,
+          email: update.email,
+          pet_image: update.image,
+          maximum_donation: update.max_donation,
+          last_date_of_donation: update.last_date,
+          short_description: update.shorts_des,
+        }
+      }
+      const result = await campaigntCollection.updateOne(query, updateUser, options)
+      res.send(result)
+    })
+    app.patch('/update/status/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const options = { upsert: true }
+      const updateStatus = {
+        $set: {
+          status: 'Adopted',
+        }
+      }
+      const result = await patCollection.updateOne(query, updateStatus, options)
+      res.send(result)
+    })
+    app.put('/update/campaign/status/:id', verifyToken, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const options = { upsert: true }
+      const info = await campaigntCollection.findOne(query)
+      let stat = '';
+      if (info.status === 'unpause') {
+        stat = 'pause'
+      }
+      else {
+        stat = 'unpause'
+      }
+      const updateStatus = {
+        $set: {
+          status: stat,
+        }
+      }
+      const result = await campaigntCollection.updateOne(query, updateStatus, options)
+      res.send(result)
+    })
+    app.post('/addpet', async (req, res) => {
+      const info = req.body;
+      console.log(info)
+      const result = await patCollection.insertOne(info)
+      res.send(result)
     })
     app.post('/users', async (req, res) => {
       // user create here when register and check is user exist or not
@@ -188,7 +188,7 @@ async function run() {
       if (req.query.q) {
         search = req.query.q
       }
-     
+
       const mySort = { date: -1 }
       const result = await patCollection.find({
         title: { $regex: search, $options: 'i' }
@@ -196,12 +196,12 @@ async function run() {
 
       res.send(result)
     })
-    
-    app.post('/adopt', async (req,res)=>{
-         const info=req.body
-         console.log(info)
-         const result= await adoptCollection.insertOne(info)
-         res.send(result)
+
+    app.post('/adopt', async (req, res) => {
+      const info = req.body
+      console.log(info)
+      const result = await adoptCollection.insertOne(info)
+      res.send(result)
     })
 
     app.get('/campaign', async (req, res) => {
@@ -209,39 +209,39 @@ async function run() {
       res.send(result)
     })
     app.get('/campaign/:id', async (req, res) => {
-      const id=req.params.id
+      const id = req.params.id
       const query = { _id: new ObjectId(id) };
       const result = await campaigntCollection.findOne(query)
       res.send(result)
     })
 
-    app.get('/adoption_data',verifyToken,async (req,res)=>{
-        const result=await adoptCollection.find().toArray()
-        res.send(result)
-    })
-    app.delete('/adoption_data/:id',async (req,res)=>{
-        const id=req.params.id;
-        const query={_id:new ObjectId(id)}
-        const result=await adoptCollection.deleteOne(query)
-        res.send(result)
-    })
-    app.get('/adoption_data/:id',async (req,res)=>{
-      const id=req.params.id;
-      const query={_id:new ObjectId(id)}
-      const result=await adoptCollection.findOne(query)
+    app.get('/adoption_data', verifyToken, async (req, res) => {
+      const result = await adoptCollection.find().toArray()
       res.send(result)
     })
-    app.get('/mycampaign',verifyToken, async (req, res) => {
+    app.delete('/adoption_data/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await adoptCollection.deleteOne(query)
+      res.send(result)
+    })
+    app.get('/adoption_data/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await adoptCollection.findOne(query)
+      res.send(result)
+    })
+    app.get('/mycampaign', verifyToken, async (req, res) => {
       const result = await campaigntCollection.find().toArray()
       res.send(result)
     })
-   
+
     app.post("/create-payment-intent", async (req, res) => {
-      const  price  = req.body;
-      
-      const int1=price.sum
-      const amount=parseInt(int1*100);
-      
+      const price = req.body;
+
+      const int1 = price.sum
+      const amount = parseInt(int1 * 100);
+
       const paymentIntent = await stripe.paymentIntents.create({
         amount: amount,
         currency: 'usd',
@@ -257,11 +257,42 @@ async function run() {
     app.post('/jwt', async (req, res) => {
       // token provider api
       const user = req.body;
-      
+
       const token = jwt.sign(user, process.env.DB_TOKEN, { expiresIn: '2h' });
       res.send({ token })
     })
+    app.get('/users', verifyToken, async (req, res) => {
+      const result = await userCollection.find().toArray()
+      res.send(result)
+    })
+    app.get('/isadmin/:email', verifyToken, async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email }
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: "Unauthorize access" })
+      }
+      const user = await userCollection.findOne(query)
+      let admin = false;
+      if (user?.role) {
+        admin = true
+      }
+      else {
+        admin = false
+      }
+      res.send({ admin })
+    })
 
+    app.patch('/make_admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const updateDoc = {
+        $set: {
+          role: "admin"
+        }
+      }
+      const result =await userCollection.updateOne(filter,updateDoc)
+      res.send(result)
+    })
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
